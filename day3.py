84ef60e103d92b42c9a25d9d63c38c416b00f54b
Zulqarnain haider <gorayazulqarnain073@gmail.com>
2024-07-02 22:34:56 +0500

    add day three and four practice

---
+++ day3.py
@@ -0,0 +1,24 @@
+# car_list=["tyre","engine","petrol"]
+# print(car_list[1])
+# car_list=["tyre","engine","petrol"]
+# car_list[0]="paint"
+# print(car_list)
+# car_list.append("side mirrors")
+# print(car_list)
+# car_list.insert(2,"seats")
+# print(car_list)
+# car_list=["tyre","engine","petrol"]
+# for x in car_list:
+#     item  = x=="tyre"
+#     print(item)
+
+# print("Write Your Name")
+# name = input()
+# print("Your Age")
+# age = input()
+
+# finalResult=f"Your Name is {name} and age is {age}"
+# print(finalResult)
+thislist =["white","green","yellow"]
+# for a in thislist:
+print(thislist)
---
+++ day4.py
@@ -0,0 +1,10 @@
+thislist=["apple","orange","mango"]
+i=0
+while i < len(thislist):
+    print(thislist[i])
+    i=i+1
+
+
+
+# for x in thislist:
+# print(x)
