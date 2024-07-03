12c6bc87804a19880001352af42b02dea5b5fa7d
Zulqarnain haider <gorayazulqarnain073@gmail.com>
2024-07-03 22:44:24 +0500

    day 6 practice

---
+++ day6.py
@@ -0,0 +1,17 @@
+# set={"apple","banana","orange"}
+
+# print(set[1])
+# myset = ["apple", "banana", "cherry"]
+my_set="apple"
+print(type(my_set))
+
+
+
+
+
+
+
+# subtuple=("mango","amrood")
+# final_tuple=maintuple+subtuple
+
+# print(final_tuple)
