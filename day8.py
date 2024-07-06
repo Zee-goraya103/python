c0ca662af3184b85cea6b8c3e3e5bedc18efb7a4
Zulqarnain haider <gorayazulqarnain073@gmail.com>
2024-07-06 23:09:41 +0500

    day 8 practice python

---
+++ day8.py
@@ -0,0 +1,9 @@
+print("PLease Enter Your Age")
+age =float( input())
+ageType= type(age)
+print(ageType)
+agePlus = age+30
+print(agePlus)
+agrminus = age-agePlus
+print(agrminus)
+print(f"Your age is {age}")
