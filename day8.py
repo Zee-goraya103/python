290bc18da66513e6aa4e33001f3cfe82a3cc2279
Zulqarnain haider <gorayazulqarnain073@gmail.com>
2024-07-08 21:45:59 +0500

    practice python

--- day8.py
+++ day8.py
@@ -1,9 +1,20 @@
-print("PLease Enter Your Age")
-age =float( input())
-ageType= type(age)
-print(ageType)
-agePlus = age+30
-print(agePlus)
-agrminus = age-agePlus
-print(agrminus)
-print(f"Your age is {age}")
+# print("PLease Enter Your Age")
+# age =float( input())
+# ageType= type(age)
+# print(ageType)
+# agePlus = age+30
+# print(agePlus)
+# agrminus = age-agePlus
+# print(agrminus)
+# print(f"Your age is {age}")
+# print("whats your favorite car?")
+# car = int(input())
+# cartype= type(car)
+# print(cartype)
+thisdict = {
+    "brand":"BMW",
+    "color":"white",
+    "model":"Iseven",
+}
+X = thisdict.get("color")
+print(X)
